f83cc0815a16792b6f1be1feb9364ba274085772
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-01-08 17:47:54 +0500

    full microservice test deployment 1 | py 3.9 typing issue remaining

--- next-fast-api-app/api/index.py
+++ next-fast-api-app/api/index.py
@@ -1,13 +1,101 @@
-from fastapi import FastAPI
-from .web import hello
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import  OAuth2PasswordRequestForm
+
+from uuid import UUID
+
+from .data.db_config import get_db
+from .models.user_auth import RegisterUser, UserOutput, LoginResonse
+from .service.user_auth import service_signup_users, service_login_for_access_token
+from .data.db_config import get_db
+from .models.todo_crud import TODOBase, TODOResponse
+from .service.todos_crud import create_todo_service, get_todo_by_id_service, get_all_todos_service, full_update_todo_service, partial_update_todo_service, delete_todo_data
+from .utils.helpers import get_current_user_dep
 
 app = FastAPI()
-app.include_router(hello.router)
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+# Hello World
+@app.get("/api", tags=["Welcome"])
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/api")
-def get_todsos():
-    return {"route": "api"}
+# user_auth.py web layer routes
+@app.post("/api/auth/login", response_model=LoginResonse, tags=["Authentication"])
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)
+):
+    
+    return await service_login_for_access_token(form_data, db)
+
+
+@app.post("/api/auth/signup/", response_model=UserOutput, tags=["Authentication"])
+async def signup_users(
+    user_data: RegisterUser, db: Session = Depends(get_db)
+):
+    return await service_signup_users(user_data, db)
+
+#  todos_crud.py web layer routes
+
+# Get ALL TODOS
+@app.get("/api/todos", response_model=list[TODOResponse], tags=["TODO Crud"])
+def get_todos(db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_dep)):
+    try:
+        return get_all_todos_service(db, user_id)
+    except Exception as e:
+        # Handle specific exceptions with different HTTP status codes if needed
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+# Get a Single TODO item
+@app.get("/api/todos/{todo_id}", response_model=TODOResponse, tags=["TODO Crud"])
+def get_todo_by_id(todo_id: UUID, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_dep)):
+    try:
+        return get_todo_by_id_service(todo_id, db, user_id)
+    except HTTPException as e:
+        # If the service layer raised an HTTPException, re-raise it
+        raise e
+    except Exception as e:
+        # Handle specific exceptions with different HTTP status codes if needed
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+
+# Create a new TODO item
+@app.post("/api/todos", response_model=TODOResponse, tags=["TODO Crud"])
+def create_todo(todo: TODOBase, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_dep)):
+    try:
+        return create_todo_service(todo, db, user_id)
+    except Exception as e:
+        # Handle specific exceptions with different HTTP status codes if needed
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+# Update a Single TODO item Completly
+
+
+@app.put("/api/todos/{todo_id}", response_model=TODOResponse, tags=["TODO Crud"])
+def update_todo(todo_id: UUID, updated_todo: TODOBase, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_dep)):
+    try:
+        return full_update_todo_service(todo_id, updated_todo, db, user_id)
+    except Exception as e:
+        # Handle specific exceptions with different HTTP status codes if needed
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+
+# Update a Single TODO item partially
+@app.patch("/api/todos/{todo_id}", response_model=TODOResponse, tags=["TODO Crud"])
+def update_todo_partial(todo_id: UUID, updated_todo: TODOBase, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_dep)):
+    try:
+        return partial_update_todo_service(todo_id, updated_todo, db, user_id)
+    except Exception as e:
+        # Handle specific exceptions with different HTTP status codes if needed
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+
+# DELETE a single TODO item
+@app.delete("/api/todos/{todo_id}", tags=["TODO Crud"])
+def delete_todo(todo_id: UUID, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_dep)):
+    try:
+        return delete_todo_data(todo_id, db, user_id)
+    except Exception as e:
+        # Handle specific exceptions with different HTTP status codes if needed
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
