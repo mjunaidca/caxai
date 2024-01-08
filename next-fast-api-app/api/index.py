80757abce3e2a5cefd3b736326bf8f90badfced1
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-01-08 16:51:18 +0500

    vercel dep test multiroute with calai deps

--- next-fast-api-app/api/index.py
+++ next-fast-api-app/api/index.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from .web import hello
 
 app = FastAPI()
+app.include_router(hello.router)
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api")
+def get_todsos():
+    return {"route": "api"}
