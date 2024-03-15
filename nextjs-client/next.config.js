0b9abfbd1eb0dc3385910dfa7596fbdd7055fb9f
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-03-16 00:21:46 +0500

    nextjs-client: rm fastapi built in todo api from nextjs service

--- nextjs-client/next.config.js
+++ nextjs-client/next.config.js
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    rewrites: async () => {
-        return [
-          {
-            source: "/api/:path*",
-            destination:
-              process.env.NODE_ENV === "development"
-                ? "http://127.0.0.1:8000/api/:path*"
-                : "/api/",
-          },
-          {
-            source: "/api/docs",
-            destination:
-              process.env.NODE_ENV === "development"
-                ? "http://127.0.0.1:8000/docs"
-                : "/api/docs",
-          },
-          {
-            source: "/openapi.json",
-            destination:
-              process.env.NODE_ENV === "development"
-                ? "http://127.0.0.1:8000/openapi.json"
-                : "/api/openapi.json",
-          },
-        ];
-      },
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
